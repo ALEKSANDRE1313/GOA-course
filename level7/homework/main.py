dcec92566fcddcef70652612c7ac3d866bfab6bc
ALEKSANDRE1313 <167910717+ALEKSANDRE1313@users.noreply.github.com>
2024-06-27 13:41:23 +0400

    Add files via upload

---
+++ level7/homework/main.py
@@ -0,0 +1,32 @@
+# 1
+
+# name = "aleksandre"
+# surname = "iashvili"
+# age = "10"
+# fav_car = "audi"
+# fav_color = "witeli"
+# fav_number = "10"
+
+# print("chemi saxeli da gvaria" + " " + name + " " + surname + ", " + "me var" + " " + age + "wlis" + ", " + "chemi sayvareli manqanaa" + " " + fav_car + ", " + "chemi savareli feria" + " " + fav_color + ", " + "chemi sayvareli ricxvia" + " " + fav_number)
+
+# 2
+
+a = int(input("pirveli ricxvi: "))
+b = int(input("meore ricxvi: "))
+c = int(input("mesame ricxvi: "))
+
+print(a + b - c)
+print(a + b + c)
+print(a + b / c)
+print(a + b * c)
+print(a - b + c)
+print(a - b - c)
+print(a - b * c)
+print(a - b / c)
+print(a / b - c)
+print(a / b * c)
+print(a / b / c)
+print(a / b + c)
+print(a * b - c)
+print(a * b + c)
+print(a * b * c)
